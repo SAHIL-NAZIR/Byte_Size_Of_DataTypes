941b808b222818fe0eb9a6cc1ccd5e157b30c99a
SAHIL NAZIR <74105412+SAHIL-NAZIR@users.noreply.github.com>
2021-09-20 21:12:16 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("byte size of int = %d\n", sizeof(int));
+    printf("byte size of char = %d\n", sizeof(char));
+    printf("byte size of long = %d\n", sizeof(long));
+    printf("byte size of long long = %d\n", sizeof(long long));
+    printf("byte size of float = %d\n", sizeof(float));
+    printf("byte size of double = %d\n", sizeof(double));
+    printf("byte size of long double = %d\n", sizeof(long double));
+    return 0;
+}
